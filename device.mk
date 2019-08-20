7afb4f4f3a2dcab6b68c9cd06dc9c27cbde00ba0
ggow <joel.bacchus@protonmail.com>
2019-08-20 15:01:41 +0100

    device: introducing mustang-vendor.mk

--- device.mk
+++ device.mk
@@ -91,4 +91,4 @@ $(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
 # Get non-open-source specific aspects
-$(call inherit-product-if-exists, $(VENDOR_COMMON)/mt8163-common-vendor.mk)
+$(call inherit-product-if-exists, $(VENDOR_COMMON)/mustang-vendor.mk)
